4322f2d8d040d5e6258ca932719a887aeb8223af
Krissy Conant <krissy@memberclicks.com>
2018-04-26 18:33:05 -0400

    looking into JS

--- cardConstructor.js
+++ cardConstructor.js
@@ -1,10 +1,23 @@
-function Card (point, suit) {
+function Card(point, suit) {
     this.point = point;
     this.suit = suit;
 };
 
-Card.prototype.render = function() {
+Card.prototype.getImageURL = function() {
     return "<img src = 'images/card.png' /> "
+}
+
+function Hand() {
+    this.hand = [];
+}
+
+Hand.prototype.addCard = function(card) {
+    this.hand.push(card);
+    this.points += card.point;
+};
+
+Hand.prototype.getPoints = function() {
+    return this.points;
 }
 
 function Deck() {
@@ -16,6 +29,21 @@ function Deck() {
         var newCard = (newPoint, newSuit);
         this.deck.push(newCard);
     };
+};
+
+Deck.prototype.draw = function() {
+    var drawCard = this.deck.pop();
+    return drewCard;
+};
+
+Deck.prototype.shuffle = function() {
+    return this.deck.sort(function(a,b) {
+        return .5 - Math.random()
+    });
+};
+
+Deck.prototype.numCardsLeft = function() {
+    return this.deck.length;
 };
 
 var myCard = new Card(point,suit);
